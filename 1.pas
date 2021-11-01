31416c9e6d5967dd3134f30034763dff070cc8ac
olgavasina-www <olgavasgo@yandex.ru>
2021-11-01 18:43:59 +0300

    buble sort

---
+++ 1.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var arr: array [1..6] of string = ('я', 'б', 'л', 'о', 'к', 'о');
+  var i : integer;
+  var str : string = '';
+  for i := 1 to 6 do
+  begin
+    str := str + arr[i]
+  end;
+  writeln(str);
+end.
---
+++ 2.pas
@@ -0,0 +1,35 @@
+﻿//алгоритм сортировки массива
+begin
+  var arr: array[1..10] of integer;
+  var i: integer;
+  var max: integer;
+  var indOfMax: integer;
+  var j : integer;
+  
+  for i:= 1 to 10 do
+  begin
+    arr[i] := random(100);
+  end;
+  
+  for i:= 1 to 10 do
+  begin
+    max := arr[i];
+    indOfMax := i;
+    for j := i to 10 do
+    begin
+      if arr[j] > max then
+      begin
+        max := arr[j];
+        indOfMax := j;
+      end;
+      if j = 10 then
+      begin
+        var temp : integer := arr[i];
+        arr[i] := max;
+        arr[indOfMax] := temp;
+        break;
+      end;
+    end;
+  end;
+  writeln(arr);
+end.
