ed83d9541b2438bade04138ac806e0e0d77f024d
olgavasina-www <olgavasgo@yandex.ru>
2021-11-04 16:33:32 +0300

    main

---
+++ mod.pas
@@ -0,0 +1,27 @@
+﻿//function getDivisionRemainder(arr: array of integer): integer;
+//begin
+//  
+//end;
+
+begin
+  var arr: array[1..1000] of integer;
+  var i: integer;
+  var sum: integer; 
+  var numStr: string;
+  
+  for i := 1 to 1000 do
+  begin
+    arr[i] := random(30000);
+  end;
+  
+  for i := 1 to 1000 do
+  begin
+    if arr[i] mod 221 = 5 then
+    begin
+      sum := sum + 1;
+      numStr := numStr + arr[i] + '; ';
+    end;
+  end;
+  writeln(sum);
+  writeln(numStr);
+end.
